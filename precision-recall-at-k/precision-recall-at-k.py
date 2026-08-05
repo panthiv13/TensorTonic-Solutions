58ab55459c5ed4e0fda8311c86788e0442a1529d
panthiv13 <panthi.vagadiya@gmail.com>
2026-08-06 01:43:48 +0530

    Solve precision-recall-at-k

--- precision-recall-at-k/precision-recall-at-k.py
+++ precision-recall-at-k/precision-recall-at-k.py
@@ -3,12 +3,7 @@ def precision_recall_at_k(recommended, relevant, k):
     Compute precision@k and recall@k for a recommendation list.
     """
     top_k = recommended[:k]
-
-    hits = 0
-
-    for item in top_k:
-        if item in relevant:
-            hits += 1
+    hits = sum(item in relevant for item in top_k)
 
     precision = hits / k
     recall = hits / len(relevant)
